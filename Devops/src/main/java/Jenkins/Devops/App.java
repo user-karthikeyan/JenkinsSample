c0a0ebd2f2e1cf0756596c0888c467cacb35c346
unixlab <unixlab@unixlab>
2024-05-31 11:59:03 +0530

    Updated Code

---
+++ Devops/src/main/java/Jenkins/Devops/App.java
@@ -0,0 +1,13 @@
+package Jenkins.Devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
